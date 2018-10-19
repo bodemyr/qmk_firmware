3660f94c224fd43761a2a58d9db59cf70062ab86
Oskar Bodemyr <oskar.bodemyr@gmail.com>
2018-10-19 19:49:50 +0200

    Fix ISO Raxxo keymap

--- keyboards/satan/keymaps/raxxo/keymap.c
+++ keyboards/satan/keymaps/raxxo/keymap.c
@@ -84,40 +84,26 @@ extern rgblight_config_t rgblight_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Keymap BASE: (Base Layer) Default Layer
-   * ,-----------------------------------------------------------.
-   * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
-   * |-----------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  R  |
-   * |-----------------------------------------------------------|
-   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '| # | R |
-   * |-----------------------------------------------------------|
-   * |Shft| \ |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
-   * |-----------------------------------------------------------|
-   * |Ctrl|Gui |Alt |      Space            |Alt |Gui |FN  |Ctrl |
-   * `-----------------------------------------------------------|
-   */
-
-[ BASE ] = KEYMAP_ISO_SPLITRSHIFT(
-  KC_ESC,         KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,      KC_BSPC, \
-  KC_TAB,         KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,     XXXXX,    \
-  MO(CAPS),      KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT, KC_NUHS,     KC_ENT,  \
-  KC_LSPO,        KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSPC,     XXXXX, \
-  KC_LCTL,        KC_LGUI,KC_LALT,                KC_SPC,                     KC_RALT,KC_RGUI,MO(FN1),    KC_RCTL),
+[ BASE ] = LAYOUT_60_iso( \
+  KC_ESC,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,    KC_MINS, KC_EQL,      KC_BSPC, \
+  KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_LBRC, KC_RBRC,    \
+  MO(CAPS), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT, KC_NUHS,     KC_ENT,  \
+  KC_LSPO,  KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT,  KC_SLSH,              KC_RSPC, \
+  KC_LCTL,  KC_LGUI,KC_LALT,                    KC_SPC,                             KC_RALT, KC_RGUI, MO(FN1),     KC_RCTL),
 
 
-[ CAPS ] = KEYMAP( //Mostly mouse layer with option for caps too
-        _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7,KC_F8, KC_F9,KC_F10,KC_F11, KC_F12, DELE, DELE,\
-        _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, HOME   ,   KC_UP, ENDI, PGUP, _______, _______, _______,\
-        _______, _______, VOLD, VOLU, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, PGDN, _______, _______, _______,\
-        KC_LSPO, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, XXXXX,\
-        KC_LCTL, KC_LGUI, KC_LALT,           _______,             _______, _______, _______, _______),
+[ CAPS ] = LAYOUT_60_iso( //Mostly mouse layer with option for caps too
+  _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10, KC_F11, KC_F12,   DELE,\
+  _______,  KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, HOME   , KC_UP,   ENDI,     PGUP,   _______, _______,\
+  _______,  _______, VOLD,    VOLU,    _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, PGDN,   _______, _______, _______,\
+  KC_LSPO,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
+  KC_LCTL,  KC_LGUI, KC_LALT,          _______,                                     _______, _______, _______, _______),
 
 
-[ FN1 ] = KEYMAP( //Functions/settings
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
-        KC_RTOG,KC_RMOD,KC_RHUI,KC_RSAI,KC_RVAI,  _______, _______, _______, _______, _______, _______, _______, _______, _______,\
-        _______,KC_DBUG,KC_RHUD,KC_RSAD,KC_RVAD, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, XXXXX,\
-        _______, _______, _______,           _______,           _______, _______, _______, _______),
+[ FN1 ] = LAYOUT_60_iso( //Functions/settings
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
+  KC_RTOG,KC_RMOD,KC_RHUI,KC_RSAI,KC_RVAI,  _______, _______, _______, _______, _______, _______, _______, _______,\
+  _______,KC_DBUG,KC_RHUD,KC_RSAD,KC_RVAD, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
+  _______, _______, _______,           _______,           _______, _______, _______, _______),
 };
