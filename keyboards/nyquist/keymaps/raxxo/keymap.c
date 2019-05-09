20185c9fbb44eea3106245df12e874ffb1c531c5
Oskar Bodemyr <oskar.bodemyr@trioptima.com>
2019-05-09 13:50:21 -0400

    Add work config

--- keyboards/nyquist/keymaps/raxxo/keymap.c
+++ keyboards/nyquist/keymaps/raxxo/keymap.c
@@ -11,12 +11,14 @@ extern keymap_config_t keymap_config;
 #define _QWERTY 0
 #define _LOWER 3
 #define _RAISE 4
+#define _MISC 5
 #define _ADJUST 16
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
+  MISC,
   ADJUST,
 };
 
@@ -42,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  MISC  ,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-  _______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, _______, _______, _______ \
+  _______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, _______, _______, KC_RBRC \
 ),
 
 /* Lower
@@ -61,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT( \
-  KC_F1  ,   KC_F2,   KC_F3, KC_F4  ,   KC_F5, KC_F6  ,   KC_F7,   KC_F8,   KC_F9, KC_MINS,  KC_EQL,  KC_DEL, \
+  KC_F1  , KC_F2  ,   KC_F3, KC_F4  ,   KC_F5, KC_F6  ,   KC_F7,   KC_F8,   KC_F9, KC_MINS,  KC_EQL,  KC_DEL, \
   KC_GRV , KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_NUBS, LSFT(KC_NUBS), KC_HOME, KC_UP  , KC_END , KC_PGUP, _______, \
   _______, _______, KC_VOLD, KC_VOLU, _______, LALT(KC_8), LALT(KC_9), KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_BSLS, \
   _______, KC_NUBS, _______, _______, LSFT(KC_8), LSFT(LALT(KC_8)), LSFT(LALT(KC_9)), LSFT(KC_9), KC_F10, KC_F11, KC_F12, _______, \
@@ -87,6 +89,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, KC_VOLD, KC_VOLU, _______, LALT(KC_8), LALT(KC_9), KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_BSLS, \
   _______, KC_NUBS, _______, _______, LSFT(KC_8), LSFT(LALT(KC_8)), LSFT(LALT(KC_9)), LSFT(KC_9), KC_F10, KC_F11, KC_F12, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+),
+
+[_MISC] = LAYOUT( \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, KC_7   , KC_8   , KC_9   , _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, KC_4   , KC_5   , KC_6   , _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, KC_1   , KC_2   , KC_3   , _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, KC_0   , _______, _______, _______, _______ \
 ),
 
 /* Adjust (Lower + Raise)
@@ -143,6 +153,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case MISC:
+        if (record->event.pressed) {
+          layer_on(_MISC);
+          update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        } else {
+          layer_off(_MISC);
+          update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        }
+        return false;
+        break;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
